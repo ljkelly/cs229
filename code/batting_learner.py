4fdeef8f73c147a8a3d25514895da7858f23e3e4
Matthew SK Lee <mattskl@stanford.edu>
2018-12-10 04:48:57 -0800

    Add LSTM, figuring out hyperparams and how to decouple time steps from prediction.

--- code/batting_learner.py
+++ code/batting_learner.py
@@ -13,6 +13,13 @@ import sys
 import json
 from sklearn.linear_model import LinearRegression
 import pdb
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch import optim
+import random
+
 # from sklearn import svm
 
 def clean_batting_dataset(attr_subset, csv_path='../baseballdatabank/Batting.csv'):
@@ -53,9 +60,17 @@ def clean_batting_dataset(attr_subset, csv_path='../baseballdatabank/Batting.csv
 
     # normalize the yearIDs
     for player, data in player_data.items():
-        career_length = data.shape[0]
         first_year = data['yearID'].min()
         data['leagueYear'] = data['yearID'] - (first_year - 1)
+
+        # # TODO: Fill in gaps in career
+        # final_year = data['yearID'].max()
+        # career_length = final_year-first_year + 1
+        # if (data.shape[0] != career_length):
+        #     print("OOps!", player, data)
+        #     break
+
+        career_length = data.shape[0]
         # Career length is one of the features we want to estimate.
         data['careerLen'] = career_length
 
@@ -99,7 +114,7 @@ def write_json(data_dict, json_path):
 
     Args:
         data_dict: a dictionary of pandas DataFrams
-        json_path: the path to the file being saved to        
+        json_path: the path to the file being saved to
     """
     copy_dict = dict()
     for key, value in data_dict.items():
@@ -125,7 +140,6 @@ def read_json(json_path):
 
     return data_dict
 
-# TODO: write function/class to train
 
 # # Use Fangraph's equation for wins above replacement
 # def player_fWAR(attr_subset, player_data):
@@ -147,7 +161,6 @@ def read_json(json_path):
 #     wRAA = (wOBA-lgwOBA)/wOBAScale * AB
 #     war = pd[]
 
-# def linReg()
 
 def make_labels_and_feature_matrix(dataset):
     """ Take dataset in dictionary form and create
@@ -201,6 +214,37 @@ def make_triplet_feature_matrix(dataset, feature_set):
         i += 1
 
     return x, y
+
+# Turn player data into <career_length x 1 x num_features>
+def prep_lstm_input_tensor(player_data, max_years=100):
+    # Keep just the # of games and normalized stats
+    subattr = [2] + list(range(14,25))
+    n_features = len(subattr)
+    # Player data sometimes not ordered by increasing year..
+    true_career_len = np.amax(player_data.values[:, -2])
+    # career_len = min(max_years, true_career_len)
+
+    # # Zero-pad stats to 20 years so LTSM doens't just learn # of input steps==>output during training
+    # career_len = max(20, true_career_len)
+    # # Just use 10 years of data?
+    # career_len = 10
+
+    career_len = max(max_years, 6)
+
+    # # Produce truncated, non-zero-padded tensors for prediction phase.
+    # career_len = min(career_len, max_years)
+
+    tensor = torch.zeros(career_len, 1, n_features)
+
+    # If didn't play in a year, stats are 0
+    for i in range(player_data.shape[0]):
+        year = player_data.values[i, -2]
+        if (year > max_years or year > career_len):
+            break
+        tensor[year-1][0][:] = torch.from_numpy(player_data.values[i, subattr].astype(float))
+
+    return true_career_len, tensor
+
 
 def linear_test(train, test):
     """Train a linear model, test it, plot the results
@@ -262,9 +306,156 @@ def three_year_test(train, test):
     plt.ylabel('Predicted number of years in the league')
     plt.title('Linear Regression on three years\' data comparison')
     plt.savefig('output/triplet_reg_output.png')
+    plt.clf()
+
+
+## From http://www.jessicayung.com/lstms-for-time-series-in-pytorch/
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1,
+                    num_layers=1):
+        super(LSTM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+
+        # TODO: Relu the output?
+
+    def init_hidden(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both
+        # have shape (num_layers, batch_size, hidden_dim).
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        return y_pred.view(-1)
+
+
+def lstm_test(train, test):
+    ## Select # hidden based on this: https://datascience.stackexchange.com/questions/10615/number-of-parameters-in-an-lstm-model
+    n_hidden = 64
+
+    learning_rate = 0.0005
+    n_epochs = 50
+    n_features = len([2] + list(range(14,25)))
+    n_train = len(train.keys())
+
+    tensors = []
+    career_lens = []
+
+    # Prep training tensors
+    for key in train.keys():
+        career_len, p_tensor = prep_lstm_input_tensor(train[key], 3)
+        tensors.append(p_tensor)
+        career_lens.append(career_len)
+    n_tensors = len(tensors)
+
+
+    # Create model:
+    # n_features for each input timestep
+    # output single value
+    # 1 LSTM layer (and 1 FC layer to convert stats to career length)
+    model = LSTM(n_features, n_hidden, batch_size=1, output_dim=1, num_layers=1)
+
+    # Use MSE since we're looking at career length match.
+    loss_fn = torch.nn.MSELoss()
+    optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Train LSTM model
+    hist = np.zeros(n_epochs)
+
+    ## From http://www.jessicayung.com/lstms-for-time-series-in-pytorch/
+    for t in range(n_epochs):
+        rr = list(range(n_tensors))
+        random.shuffle(rr)
+        j = 0
+        for i in rr:
+            # Clear stored gradient
+            model.zero_grad()
+
+            # Initialise hidden state
+            # Don't do this if you want your LSTM to be stateful
+            model.hidden = model.init_hidden()
+
+            # Forward pass
+            y_pred = model(tensors[i])
+
+            loss = loss_fn(y_pred, torch.tensor(float(career_lens[i])))
+            hist[t] += loss.item()
+
+            # Zero out gradient, else they will accumulate between epochs/samples
+            optimiser.zero_grad()
+
+            # Backward pass
+            loss.backward()
+
+            # Update parameters
+            optimiser.step()
+
+            if j % 1000 == 0:
+                print("Epoch ", t, "item ", j, "MSE: ", loss.item())
+            j += 1
+        hist[t] /= float(n_tensors)
+        print("Epoch ", t, "average MSE: ", hist[t])
+
+
+    test_tensors = []
+    test_career_lens = []
+
+    # Prep test tensors - only first 3 years of timeseries.
+    for key in test.keys():
+        career_len, p_tensor = prep_lstm_input_tensor(test[key], 3)
+        test_tensors.append(p_tensor)
+        test_career_lens.append(career_len)
+    n_test_tensors = len(test_tensors)
+
+    test_preds = []
+    for i in range(n_test_tensors):
+        # Reset hidden state between predictions
+        model.hidden = model.init_hidden()
+
+        # Forward pass
+        y_pred = model(test_tensors[i])
+        test_preds.append(y_pred.item())
+
+    test_preds = np.asarray(test_preds)
+    test_career_lens = np.asarray(test_career_lens)
+    deltas = test_preds-test_career_lens
+
+    np.savetxt("./output/lstm_epoch_err.txt", hist)
+    np.savetxt("./output/lstm_prediction.txt", test_preds)
+    np.savetxt("./output/lstm_labels.txt", test_career_lens)
+    np.savetxt("./output/lstm_deltas.txt", deltas)
+    print('Average error: ', np.mean(abs(deltas)))
+
+    poly = np.polyfit(test_career_lens, test_preds, 1)
+    y_reg = poly[0] * test_career_lens + poly[1]
+    plt.scatter(test_career_lens, test_preds)
+    plt.plot(test_career_lens, y_reg, 'r')
+    plt.xlabel('True number of years in the league')
+    plt.ylabel('Predicted number of years in the league')
+    plt.title('LSTM data comparison')
+    plt.savefig('output/lstm_output.png')
+
 
 if __name__ == '__main__':
     np.random.seed(229)
+    torch.manual_seed(229)
+    random.seed(229)
 
     reprune = 0 if len(sys.argv) <= 1 else int(sys.argv[1])
 
@@ -287,3 +478,4 @@ if __name__ == '__main__':
 
     linear_test(train, test)
     three_year_test(train, test)
+    lstm_test(train, test)
